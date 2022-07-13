0e7a0c5705819e51716a9756dce05a96fb92bafe
Luca_Ferri <lucaferrip@gmail.com>
2022-07-13 08:15:11 -0300

    Adicionando jogos no array de jogos

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("A aplicação está se conectando ao banco de dados");
 
 const jogador = 'Luca';
-const jogos = [];
+const jogosCadastrados = ['AngryBirds', 'Contexto', 'Termo'];
